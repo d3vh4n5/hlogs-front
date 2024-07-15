d73975096ac916d3a82716e2d4f9254552fd3fd3
Juan Basgall <juanangelbasgall@hotmail.com>
2024-07-15 16:21:13 -0300

    Probando variables de entorno

--- src/constants/apiURL.js
+++ src/constants/apiURL.js
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.API_URL || "http://localhost:5000/api/v1"
-export const BASE_API_URL = import.meta.env.BASE_API_URL || "http://localhost:5000"
+export const API_URL = import.meta.env.PUBLIC_API_URL || "http://localhost:5000/api/v1"
+export const BASE_API_URL = import.meta.env.PUBLIC_BASE_API_URL || "http://localhost:5000"
